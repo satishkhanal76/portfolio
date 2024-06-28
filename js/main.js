47bdcee96248d6ce708025ba2cea66f547b462b0
SatishKhanal76 <satishkhanal76@gmail.com>
2024-06-28 03:13:01 -0400

    Added theme toggle on dblclick.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 document.getElementById("theme-toggle").addEventListener("click", () => {
   document.body.classList.toggle("light-theme");
 });
+
+document.body.addEventListener("dblclick", () => {
+  document.body.classList.toggle("light-theme");
+});
